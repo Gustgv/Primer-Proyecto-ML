cdfef66fbd504f4154249c72a78ddbd495cda965
Gustgv <111980132+Gustgv@users.noreply.github.com>
2023-02-19 23:40:25 -0400

    Update reco_movies.py

--- reco_movies.py
+++ reco_movies.py
@@ -48,11 +48,11 @@ st.header('Por favor ingrese nro de usuario e id de la pelicula:')
 user = st.number_input('Id de Usuario (Maximo 124380):', min_value=1, max_value=124380, value=1)
 
 movie = st.text_input('Escriba Id de la pelicula, Ej: ns405, ds693, as288, hs789')
-st.write('El titulo de la pelicula es', movie)
+st.write('El titulo de la pelicula es', title[title.id == movie].iloc[0,1])
 
 scoring = st.slider('Especifique la calificacion esperada', 1, 5, 1)
 st.write('Puntuacion esperada:', scoring)
 
 
 if st.button('Recomendar'):
-    st.success(print(recommendation(user, movie, scoring)))
+    print(recommendation(user, movie, scoring))
